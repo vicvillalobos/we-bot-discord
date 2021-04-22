9611475ff8c94de2e589b2530768085ed118cf6c
Victor Villalobos <vvillalobos@we-techs.com>
2021-04-22 17:13:55 -0400

    Added basic command functionality

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 require('dotenv').config();
 
 const Discord = require('discord.js');
+const Command = require('./commands');
 
-const client = new Discord.Client();
+const client = new Discord.Client({partials: [ "MESSAGE", "CHANNEL", "REACTION" ]});
+
+const prefix = "--";
 
 const token = process.env.DISCORD_TOKEN;
 console.log("DISCORD_TOKEN: ",token);
@@ -12,6 +15,16 @@ client.once('ready', () => {
     console.log('WeBot is online!');
 });
 
+client.on('message', message => {
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/\s+/);
+    const command = args.shift().toLowerCase();
+
+    const cmd = new Command(command);
+
+    cmd.execute(message);
+});
 
 // This has to be at the end of the file!
 client.login(token);
